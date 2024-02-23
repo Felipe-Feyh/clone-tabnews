38deb2470d45ca84bbabdc3a6eeed9e512656c67
Felipe Feyh <38591330+Felipe-Feyh@users.noreply.github.com>
2024-02-23 00:42:53 +0000

    Ajustar página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Teste de Continuous Deployment</h1>;
+  return (
+    <div>
+      <h1>Em construção</h1>
+      <img
+        src="https://jcbautomec.com.br/wp-content/uploads/2023/04/retroescavadeira_jcb_01-1024x585.jpg.webp"
+        alt="Retroescavadeira"
+      />
+    </div>
+  );
 }
 
 export default Home;
