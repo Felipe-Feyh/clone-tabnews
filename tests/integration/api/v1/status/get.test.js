a705bbb2aa7195897af5014a01957652d8562bb7
Felipe Feyh <38591330+Felipe-Feyh@users.noreply.github.com>
2025-05-05 22:39:27 +0000

    feat: add initial version of `/status` page

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,16 +4,20 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-describe("GET /api/v1/migrations", () => {
+describe("GET /api/v1/status", () => {
   describe("Anonymous user", () => {
-    test("Retrieving pending migrations", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/migrations");
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
       expect(response.status).toBe(200);
 
-      const responseBody = await response.json();
+      //     const responseBody = await response.json();
 
-      expect(Array.isArray(responseBody)).toBe(true);
-      expect(responseBody.length).toBeGreaterThan(0);
+      //  const parsedUpdatedAt = new Date(responseBody.updatedAt).toISOString();
+      //  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      //    expect(responseBody.dependencies.database.version).toEqual("16.0");
+      //    expect(responseBody.database.max_connections).toEqual(100);
+      //    expect(responseBody.database.opened_connections).toEqual(1);
     });
   });
 });
